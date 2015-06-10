a7c5cc19d3f91b7eda90b2b7901c9db09fdf1c84
Simon Harrer <simon.harrer@gmail.com>
2015-06-10 11:29:19 +0200

    Stop graceful instead of just exit.

--- deployment-styles/standalone/src/main/java/de/uniba/dsg/soa/GreetingsEndpoint.java
+++ deployment-styles/standalone/src/main/java/de/uniba/dsg/soa/GreetingsEndpoint.java
@@ -4,14 +4,14 @@ import javax.xml.ws.Endpoint;
 import java.io.IOException;
 
 public class GreetingsEndpoint {
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		String url = "http://localhost:8080/standalone/GreetingsService";
-		
+
 		System.out.println("Starting Web Service Endpoint...");
-		Endpoint.publish(url, new GreetingsImpl()); 
+		Endpoint endpoint = Endpoint.publish(url, new GreetingsImpl());
 		System.out.println("Endpoint available at " + url);
-		System.out.println("Press any key to exit...");		
+		System.out.println("Press any key to exit...");
 		System.in.read();
-		System.exit(0);
-  }  
+		endpoint.stop();
+	}
 }
